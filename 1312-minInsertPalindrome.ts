0fa5617365550d6a19831ec287109d1e9f1fa84e
sencenan <sen.cenan@gmail.com>
2021-05-29 12:23:44 -0400

    m: code golf

--- 1312-minInsertPalindrome.ts
+++ 1312-minInsertPalindrome.ts
@@ -1,31 +1,22 @@
 namespace MinInsertPalindrome1312 {
 
-function minInsertions(s: string, lo = 0, hi = s.length - 1, memo: { [_: number]: number } = {}): number {
-    const key = lo * s.length + hi;
-    if (memo[key] !== undefined) {
-        return memo[key];
-    }
-
-    if (hi - lo <= 0) {
-        return memo[key] = 0;
-    }
-
-    for (let i = lo, j = hi; i <= j; i += 1, j -= 1) {
-        if (s[i] !== s[j]) {
-            // not a palindrome
-            return memo[key] = 1 + Math.min(
-                minInsertions(s, i + 1, j, memo),
-                minInsertions(s, i, j - 1, memo)
-            );
+function minInsertions(s: string, memo: number[] = []): number {
+    return function cost(lo = 0, hi = s.length - 1, k = lo * s.length + hi): number {
+        for (let i = lo, j = hi; memo[k] === undefined && i <= j; i += 1, j -= 1) {
+            if (s[i] !== s[j]) {
+                memo[k] = 1 + Math.min(cost(i + 1, j), cost(i, j - 1));
+            }
         }
-    }
 
-    return memo[key] = 0;
+        return memo[k] || 0;
+    }();
 };
 
 let s: string, p: string;
 
 s = "tldjbqjdogipebqsohdypcxjqkrqltpgviqtqz";
+console.log(minInsertions(s));
+s = "leetcode";
 console.log(minInsertions(s));
 
 }
