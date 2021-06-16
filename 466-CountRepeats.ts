535989934886c2e134f30891e23296afb29fb99a
sencenan <sen.cenan@gmail.com>
2021-06-15 21:19:10 -0400

    466 accepted

--- 466-CountRepeats.ts
+++ 466-CountRepeats.ts
@@ -1,35 +1,35 @@
 namespace CountCountRepeats466 {
 
-function getMaxRepetitions(s1: string, n1: number, s2: string, n2: number): number {
+    function getMaxRepetitions(s1: string, n1: number, s2: string, n2: number): number {
 
-    // test for impossible cases, s1 must contain all chars in s2
-    for (let i = 0; i < s2.length; i += 1) {
-        if (s1.indexOf(s2[i]) < 0) {
+        // test for impossible cases, s1 must contain all chars in s2
+        for (let i = 0; i < s2.length; i += 1) {
+            if (s1.indexOf(s2[i]) < 0) {
+                return 0;
+            }
+        }
+
+        // find how many s2 can fit in s1 * n1
+        let
+            n1i = 0,
+            n2i = 0,
+            str2Len = s2.length * n2,
+            s1Cnt = 0,
+            str2Cnt = 0;
+
+        if (n1 * s1.length < str2Len) {
             return 0;
         }
-    }
 
-    // find how many s2 can fit in s1 * n1
-    let
-        n1i = 0,
-        n2i = 0,
-        str2Len = s2.length * n2;
+        while (n1i < n1 * s1.length) {
+            if (s2[n2i % s2.length] === s1[n1i % s1.length]) {
+                n2i += 1;
+            }
 
-    if (n1 * s1.length < str2Len) {
-        return 0;
-    }
-
-    while (n1i < n1 * s1.length) {
-        if (s2[n2i % s2.length] === s1[n1i % s1.length]) {
-            n2i += 1;
+            n1i += 1;
         }
 
-        n1i += 1;
-
-        // TODO: early exit
-    }
-
-    return Math.floor(n2i / str2Len);
-};
+        return Math.floor(n2i / str2Len);
+    };
 
 }
